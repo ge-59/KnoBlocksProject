898a16e0f22fad14edd62b112206d4d13dfcd0fe
ge-59 <gepiscopou@gmail.com>
2023-01-11 22:25:32 +0200

    More Testo

--- test/KnoBlockIO.ts
+++ test/KnoBlockIO.ts
@@ -20,14 +20,15 @@ describe('KnoBlockIO contract', function () {
     });
 
     describe('Deployment', function () {
+      // Does not set the "owner" in KnoBlock Storage, but in Ownable. So do we even need the one in KnoBlockStorage?
       it('Should set the right owner', async function () {
         await KnoBlockIOInstance.deployed();
         const owner = await KnoBlockIOInstance.MockReturnOwner();
-        expect(deployer.address).to.equal(owner); 
+        expect(deployer.address).to.equal(owner);
       });
     });
 
-    describe('Create KnoBlock', function () { 
+    describe('Create KnoBlock', function () {
       //Is there not a way to initiate the function once for all these checks
       it('Should increment count variable by 1', async function () {
         await KnoBlockIOInstance.deployed();
@@ -70,5 +71,89 @@ describe('KnoBlockIO contract', function () {
           .withArgs(0, deployer.address, 1001, 0, 1);
       });
     });
+    describe('KnoBlock Deposit', function () {
+      it('Should Increase the KnoBlocks currentAmount by msg.value', async function () {
+        const msgvalue = { value: ethers.utils.parseEther('0.000000000000001') }; //sending 1000 wei 
+        await KnoBlockIOInstance.deployed();
+        await KnoBlockIOInstance.createKnoBlock(1001, 1);
+        await KnoBlockIOInstance.knoDeposit(0, msgvalue);
+      expect(
+        await KnoBlockIOInstance.MockReturnKnoBlockCurrentAmount(0),
+      ).to.equal(1000);
+  });
+ it('Should update msg.senders deposit value accurately', async function () {
+  const msgvalue = { value: ethers.utils.parseEther('0.000000000000001') };  //sending 1000 wei 
+  await KnoBlockIOInstance.deployed();
+  await KnoBlockIOInstance.createKnoBlock(1001, 1);
+  await KnoBlockIOInstance.knoDeposit(0, msgvalue);
+expect(
+  await KnoBlockIOInstance.MockReturnKnoBlockDeposits(0),
+).to.equal(1000);
+});
+
+it('Unlocked variable of Knoblock should be set to true if currentAmount is equal to UnlockAmount', async function () {
+  const msgvalue = { value: ethers.utils.parseEther('0.000000000000001001') };  //sending 1001 wei 
+  await KnoBlockIOInstance.deployed();
+  await KnoBlockIOInstance.createKnoBlock(1001, 1);
+  await KnoBlockIOInstance.knoDeposit(0, msgvalue);
+expect(
+  await KnoBlockIOInstance.MockReturnKnoBlockDeposits(0),
+).to.equal(1001);  // is there any value in keeping this? 
+expect(
+  await KnoBlockIOInstance.MockReturnKnoBlockUnlocked(0),
+).to.be.true;
+});
+
+it('Unlocked Event should be emitted if currentAmount is equal to UnlockAmount', async function () {
+  const msgvalue = { value: ethers.utils.parseEther('0.000000000000001001') };  //sending 1001 wei 
+  await KnoBlockIOInstance.deployed();
+  await KnoBlockIOInstance.createKnoBlock(1001, 1);
+  expect(await KnoBlockIOInstance.knoDeposit(0, msgvalue))
+          .to.emit(KnoBlockIOInstance, 'BlockUnlocked')
+          .withArgs(0, deployer.address, 1001, 1001, 1);
+
+});
+
+it('Unlocked variable of Knoblock should be set to true if currentAmount is greater to UnlockAmount', async function () {
+  const msgvalue = { value: ethers.utils.parseEther('0.000000000000002000') };  //sending 2000 wei
+  await KnoBlockIOInstance.deployed();
+  await KnoBlockIOInstance.createKnoBlock(1001, 1);
+  await KnoBlockIOInstance.knoDeposit(0, msgvalue);
+expect(
+  await KnoBlockIOInstance.MockReturnKnoBlockUnlocked(0),
+).to.be.true;
+});
+
+it('Unlocked Event should be emitted if currentAmount is equal to UnlockAmount', async function () {
+  const msgvalue = { value: ethers.utils.parseEther('0.000000000000002000') };  //sending 2000 wei
+  await KnoBlockIOInstance.deployed();
+  await KnoBlockIOInstance.createKnoBlock(1001, 1);
+  expect(await KnoBlockIOInstance.knoDeposit(0, msgvalue))
+          .to.emit(KnoBlockIOInstance, 'BlockUnlocked')
+          .withArgs(0, deployer.address, 1001, 1001, 1);
+
+});
+
+it('Should return Overkill to Msg.sender if they deposit more than the remaining amount to Unlock', async function () {
+  const msgvalue = { value: ethers.utils.parseEther('0.000000000000002000') };  //sending 2000 wei
+  await KnoBlockIOInstance.deployed();
+  await KnoBlockIOInstance.createKnoBlock(1001, 1);
+  await KnoBlockIOInstance.knoDeposit(0, msgvalue);
+expect //not todei
+});
+  
+ // Sadge //
+  it('HUH.Cant set a KnoBlocks Value in Ethers', async function () {
+    const msgvalue = { value: ethers.utils.parseEther('0.000000000000001') }; 
+    await KnoBlockIOInstance.deployed();
+    await KnoBlockIOInstance.createKnoBlock(1000000000000000000, 1);   
+    await KnoBlockIOInstance.knoDeposit(0, msgvalue);
+  expect(
+    await KnoBlockIOInstance.MockReturnKnoBlockCurrentAmount(0),
+  ).to.equal(1000);
+});
+    });
   });
 });
+
+
